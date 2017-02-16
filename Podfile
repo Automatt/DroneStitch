2641514a31fce0c4e460a63ddc7921484c602dda
Mathew Spolin <matt@automatt.com>
2017-02-16 10:59:14 -0800

    Pick images from camera roll

--- Podfile
+++ Podfile
@@ -1,15 +1,15 @@
 source 'https://github.com/CocoaPods/Specs.git'
 source 'https://github.com/foundry/Specs.git'
 
-platform :ios, '7.0'
+platform :ios, '8.0'
 
 
 # Uncomment this line if you're using Swift
 use_frameworks!
 
 
-target 'SwiftStitch' do
-    #pod 'OpenCV', '2.4.9.3'
+target 'DroneStitch' do
     pod 'OpenCV', '~> 3.1.0.1'
+    pod 'CTAssetsPickerController', :git => 'https://github.com/cdwat/CTAssetsPickerController'
 end
 
--- SwiftStitch/SwViewController.swift
+++ SwiftStitch/SwViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CTAssetsPickerController
 
-class SwViewController: UIViewController, UIScrollViewDelegate {
+class SwViewController: UIViewController, UIScrollViewDelegate, CTAssetsPickerControllerDelegate {
     
     @IBOutlet var spinner:UIActivityIndicatorView!
     @IBOutlet var imageView:UIImageView?
     @IBOutlet var scrollView:UIScrollView!
+    
+    var picked: Bool = false
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -37,7 +40,70 @@ class SwViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        stitch()
+        
+        if !picked {
+            pick()
+        }
+        //stitch()
+    }
+    
+    func pick() {
+        let picker: CTAssetsPickerController = CTAssetsPickerController()
+        picker.delegate = self
+        
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func assetsPickerControllerDidCancel(_ picker: CTAssetsPickerController) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func assetsPickerController(_ picker: CTAssetsPickerController, didFinishPicking assets: [PHAsset]) {
+        
+        picked = true
+
+        picker.dismiss(animated: true) {
+            var imgArray:[UIImage?] = []
+            let ratio:CGFloat = 0.10
+
+            for asset in assets {
+                let img = self.compressToRatio(image: self.getAssetImageThumbnail(asset: asset), ratio: ratio)
+                imgArray.append(img)
+            }
+            
+            
+            let stitchedImage:UIImage = CVWrapper.process(with: imgArray as! [UIImage]) as UIImage
+
+            DispatchQueue.main.async {
+                NSLog("stichedImage %@", stitchedImage)
+                
+                self.imageView = UIImageView.init(image: stitchedImage)
+                
+                self.scrollView.addSubview(self.imageView!)
+                self.scrollView.backgroundColor = UIColor.black
+                self.scrollView.contentSize = self.imageView!.bounds.size
+                self.scrollView.maximumZoomScale = 4.0
+                self.scrollView.minimumZoomScale = 1.0
+                self.scrollView.delegate = self
+                self.scrollView.contentOffset = CGPoint(x: -(self.scrollView.bounds.size.width - self.imageView!.bounds.size.width)/2.0, y: -(self.scrollView.bounds.size.height - self.imageView!.bounds.size.height)/2.0)
+                NSLog("scrollview \(self.scrollView.contentSize)")
+                self.spinner.stopAnimating()
+            }
+        }
+
+    }
+    
+    func getAssetImageThumbnail(asset: PHAsset) -> UIImage {
+        let manager = PHImageManager.default()
+        let option = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        option.isSynchronous = true
+        manager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+            thumbnail = result!
+        })
+        return thumbnail
     }
     
     func stitch() {
@@ -77,7 +143,6 @@ class SwViewController: UIViewController, UIScrollViewDelegate {
             }
             
             let stitchedImage:UIImage = CVWrapper.process(with: imgArray as! [UIImage]) as UIImage
-            
             
             DispatchQueue.main.async {
                 NSLog("stichedImage %@", stitchedImage)
